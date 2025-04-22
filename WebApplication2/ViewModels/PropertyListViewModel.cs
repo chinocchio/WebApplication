9e5ff5296b1a5ab360813f43b10a518893f5b99c
chinocchio <77035738+chinocchio@users.noreply.github.com>
2025-04-22 15:59:33 +0800

    Properties Search Via Unit Code

---
+++ WebApplication2/ViewModels/PropertyListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using WebApplication2.Models;
+
+namespace WebApplication2.ViewModels
+{
+    public class PropertyListViewModel
+    {
+        public IEnumerable<Property>? Properties { get; set; }
+
+        public string? SearchTerm { get; set; }
+    }
+}
