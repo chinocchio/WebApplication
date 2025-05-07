01ae73c0a54c33aa117e77755c857aa39c70943d
chinocchio <77035738+chinocchio@users.noreply.github.com>
2025-05-07 14:11:47 +0800

    AddedColumn in the submitted and for submission

---
+++ WebApplication2/Migrations/20250507055253_AddedNameToDocumentTable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNameToDocumentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DateSubmitted",
+                table: "SubmittedDocuments",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DocumentName",
+                table: "SubmittedDocuments",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DocumentName",
+                table: "DocumentForSubmissions",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateSubmitted",
+                table: "SubmittedDocuments");
+
+            migrationBuilder.DropColumn(
+                name: "DocumentName",
+                table: "SubmittedDocuments");
+
+            migrationBuilder.DropColumn(
+                name: "DocumentName",
+                table: "DocumentForSubmissions");
+        }
+    }
+}
--- WebApplication2/Models/DocumentSubmitted.cs
+++ WebApplication2/Models/DocumentSubmitted.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string? DocumentCode { get; set; }
+        public string? DocumentName { get; set; }
+        public DateOnly? DateSubmitted { get; set; }
         public long? ContractNumber { get; set; }
         public string? CustomerCode { get; set; }
         public string? UnitCode { get; set; }
