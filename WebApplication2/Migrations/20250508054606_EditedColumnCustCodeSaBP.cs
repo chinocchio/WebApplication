374615d60fcf0a569c9d59e47dde439c883f033d
chinocchio <77035738+chinocchio@users.noreply.github.com>
2025-05-08 14:01:17 +0800

    Updated bp Table

---
+++ WebApplication2/Migrations/20250508054606_EditedColumnCustCodeSaBP.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication2.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditedColumnCustCodeSaBP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerCode",
+                table: "BusinessPartners",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "CustomerCode",
+                table: "BusinessPartners",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
